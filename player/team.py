71b0ef8576185ac3d43c592f38822759ea07d6a1
Albert Gao <adbforlife2018@gmail.com>
2019-02-16 14:18:22 -0500

    winning-bot

--- player/team.py
+++ player/team.py
@@ -10,6 +10,7 @@ sixiangg
 hengz2
 """
 from awap2019 import Tile, Direction, State
+from random import *
 
 class Team(object):
     def __init__(self, initial_board, team_size, company_info):
@@ -26,7 +27,11 @@ class Team(object):
         self.team_size = team_size
         self.company_info = company_info
 
-        self.team_name = # Add your team name here!
+        self.team_name = "Null Graphs' Lives Matter"
+
+        print(initial_board)
+        print(team_size)
+        print(company_info)
 
     def step(self, visible_board, states, score):
         """
@@ -35,5 +40,26 @@ class Team(object):
         For more information on what visible_board, states, and score
         are, please look on the wiki.
         """
+        possible_dirs = [Direction.LEFT, Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.NONE, Direction.ENTER, Direction.REPLACE]
+        res = []
+        for s in states:
+            if s.line_pos != -1:
+                res.append(Direction.NONE)
+            else:
+                if Tile.is_end_of_line(visible_board[s.x][s.y]):
+                    res.append(Direction.ENTER)
+                else:
+                    res.append(possible_dirs[randint(0,3)])
+        return res
 
-        pass
+        
+
+
+
+
+
+
+
+
+
+
